25cbd683e612addb788e4bf33a15832a7fd8cc57
GERMANUS KEMING <germanuskeming@gmail.com>
2020-07-02 04:35:05 -0700

    Changed site. The new edited copy; 02/07/2020

--- js/header_footer.js
+++
@@ -1,9 +0,0 @@
-/*let header = document.getElementById("ngo-header");
-let header_style = header.style;
-
-if (document.body.scrollTop < 10) {
-    header.style.display = "none";
-}
-else{
-    header.style = header_style;
-}*/
